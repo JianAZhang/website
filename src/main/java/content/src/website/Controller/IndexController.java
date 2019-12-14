f4b3c3e7a7286fd6cc51390922422a538c65202e
JianAZhang <jianzhang_pro@foxmail.com>
2019-12-14 19:22:15 +0800

    add web content

---
+++ src/main/java/content/src/website/Controller/IndexController.java
@@ -0,0 +1,13 @@
+package content.src.website.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+
+    public String Index () {
+        return "index";
+    }
+}
