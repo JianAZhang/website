214f211c4f060d2828ae7fe8842144240e9de76e
JianAZhang <jianzhang_pro@foxmail.com>
2019-12-15 23:39:35 +0800

    update README.md

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Website
-这是我的网站 : http://www.jianzhang.vip
+这是我的网站 : https://www.jianzhang.vip
 
 目前处于第一个版本，使用spring boot开发，前端使用html，css，JavaScript三剑客完成，暂不使用前端框架。
 
--- src/main/java/content/src/website/Controller/BlogController.java
+++ src/main/java/content/src/website/Controller/BlogController.java
@@ -1,4 +1,13 @@
 package content.src.website.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class BlogController {
+    @GetMapping("/blog")
+
+    public String Blog() {
+        return "blog";
+    }
 }
